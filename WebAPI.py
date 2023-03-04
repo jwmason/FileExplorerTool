ae8aeac1aebd27c9007a180cdf44d017de43bfcf
Mason Wong <masonjw1@uci.edu>
2023-03-03 17:05:39 -0800

    started the code

---
+++ WebAPI.py
@@ -0,0 +1,30 @@
+# webapi.py
+
+# Starter code for assignment 4 in ICS 32
+# Programming with Software Libraries in Python
+
+# Replace the following placeholders with your information.
+
+# Mason Wong
+# masonjw1@uci.edu
+# 48567424
+
+from abc import ABC, abstractmethod
+
+class WebAPI(ABC):
+
+  def _download_url(self, url: str) -> dict:
+    #TODO: Implement web api request code in a way that supports
+    # all types of web APIs
+    pass
+
+  def set_apikey(self, apikey:str) -> None:
+    pass
+
+  @abstractmethod
+  def load_data(self):
+    pass
+
+  @abstractmethod
+  def transclude(self, message:str) -> str:
+    pass
