e9872d357ad5b0231d85ac5e376c6e429cbfaa5f
Mason Wong <masonjw1@uci.edu>
2023-03-09 19:53:51 -0800

    got validity checker to work for openweather.py

--- OpenWeather.py
+++ OpenWeather.py
@@ -9,3 +9,90 @@
 # masonjw1@uci.edu
 # 48567424
 
+"""This module retrieves data from the OpenWeather API."""
+
+import urllib, json
+from urllib import request,error
+import urllib.request, urllib.error
+
+
+def _download_url(url_to_download: str) -> dict:
+    response = None
+    r_obj = None
+
+    try:
+        response = urllib.request.urlopen(url_to_download)
+        json_results = response.read()
+        r_obj = json.loads(json_results)
+
+    except urllib.error.HTTPError as e:
+        print('Failed to download contents of URL')
+        print('Status code: {}'.format(e.code))
+
+    finally:
+        if response != None:
+            response.close()
+    
+    return r_obj
+
+
+class OpenWeather():
+    """This class stores data from OpenWeather API."""
+    def __init__(self, zip: str, ccode: str):
+        self.zip = zip
+        self.ccode = ccode
+        self.weather_data = {}
+
+
+    def set_apikey(self, apikey:str) -> None:
+        '''
+        Sets the apikey required to make requests to a web API.
+        :param apikey: The apikey supplied by the API service
+    
+        '''
+        #TODO: assign apikey value to a class data attribute that can be accessed by class members
+        self.apikey = apikey
+        pass
+
+
+    def load_data(self) -> None:
+        '''
+        Calls the web api using the required values and stores the response in class data attributes.
+            
+        '''
+        #TODO: use the apikey data attribute and the urllib module to request data from the web api. See sample code at the begining of Part 1 for a hint.
+        #TODO: assign the necessary response data to the required class data attributes
+        url = f"http://api.openweathermap.org/data/2.5/weather?zip={self.zip},{self.ccode}&appid={self.apikey}"
+        try:
+            response = urllib.request.urlopen(url)
+            json_results = response.read()
+            r_obj = json.loads(json_results)
+
+            self.temperature = r_obj['main']['temp']
+            self.high_temperature = r_obj['main']['temp_max']
+            self.low_temperature = r_obj['main']['temp_min']
+            self.longitude = r_obj['coord']['lon']
+            self.latitude = r_obj['coord']['lat']
+            self.description = r_obj['weather'][0]['description']
+            self.humidity = r_obj['main']['humidity']
+            self.city = r_obj['name']
+            self.sunset = r_obj['sys']['sunset']
+
+        except urllib.error.HTTPError as e:
+            print('Failed to download contents of URL')
+            print('Status code: {}'.format(e.code))
+
+
+def main() -> None:
+    zip = "92697"
+    ccode = "US"
+    apikey = "e5e0d69e2df302b25f3f486a47e42067"
+    url = f"http://api.openweathermap.org/data/2.5/weather?zip={zip},{ccode}&appid={apikey}"
+
+    weather_obj = _download_url(url)
+    if weather_obj is not None:
+        print(weather_obj)
+
+
+if __name__ == '__main__':
+    main()
---
+++ test.py
@@ -0,0 +1,17 @@
+from OpenWeather import OpenWeather
+
+zipcode = "92697"
+ccode = "US"
+apikey = "e5e0d69e2df302b25f3f486a47e42067"
+
+open_weather = OpenWeather(zipcode, ccode)
+open_weather.set_apikey(apikey)
+open_weather.load_data()
+
+print(f"The temperature for {zipcode} is {open_weather.temperature} degrees")
+print(f"The high for today in {zipcode} will be {open_weather.high_temperature} degrees")
+print(f"The low for today in {zipcode} will be {open_weather.low_temperature} degrees")
+print(f"The coordinates for {zipcode} are {open_weather.longitude} longitude and {open_weather.latitude} latitude")
+print(f"The current weather for {zipcode} is {open_weather.description}")
+print(f"The current humidity for {zipcode} is {open_weather.humidity}")
+print(f"The sun will set in {open_weather.city} at {open_weather.sunset}")
