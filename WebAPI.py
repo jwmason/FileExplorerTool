1750b9a836e4b45c1dc2eb264f962b427dea8c11
Mason Wong <masonjw1@uci.edu>
2023-03-10 17:59:57 -0800

    finished pylinting

--- WebAPI.py
+++ WebAPI.py
@@ -19,36 +19,40 @@ import json
 
 
 class WebAPI(ABC):
-  """This is the parent class for the Web API."""
-  def _download_url(self, url: str) -> dict:
-    """This function implements web api request codes
-    in a way that supports all types of web APIs."""
-    try:
-        response = urllib.request.urlopen(url)
-        json_results = response.read()
-        r_obj = json.loads(json_results)
-        return r_obj
-    except urllib.error.HTTPError as e:
-        if e.code == 404 or e.code == 503:
-            print('Error, the remote API is unavailable')
-        else:
-            print('Failed to download contents of URL')
-            print('Status code: {}'.format(e.code))
-    except urllib.error.URLError:
-        print('Error with local connection to the Internet')
-    except ValueError:
-        print('Error with invalid data formatting from the remote API')
-    except SyntaxError:
-        print('Error with invalid data formatting from the remote API')
+    """This is the parent class for the Web API."""
+    def __init__(self):
+        self.apikey = None
 
-  def set_apikey(self, apikey: str) -> None:
-    """Sets api key"""
-    self.apikey = apikey
+    def _download_url(self, url: str) -> dict:
+        """This function implements web api request codes
+        in a way that supports all types of web APIs."""
+        try:
+            response = urllib.request.urlopen(url)
+            json_results = response.read()
+            r_obj = json.loads(json_results)
+            return r_obj
+        except urllib.error.HTTPError as e:
+            if e.code in (404, 503):
+                print('Error, the remote API is unavailable')
+            else:
+                print('Failed to download contents of URL')
+                print(f'Status code: {e.code}')
+        except urllib.error.URLError:
+            print('Error with local connection to the Internet')
+        except ValueError:
+            print('Error with invalid data formatting from the remote API')
+        except SyntaxError:
+            print('Error with invalid data formatting from the remote API')
 
-  @abstractmethod
-  def load_data(self):
-    pass
+    def set_apikey(self, apikey: str) -> None:
+        """Sets api key"""
+        self.apikey = apikey
 
-  @abstractmethod
-  def transclude(self, message: str) -> str:
-    pass
+    @abstractmethod
+    def load_data(self):
+        """This abstract function loads data"""
+
+
+    @abstractmethod
+    def transclude(self, message: str) -> str:
+        """This abstract method transcludes message"""
